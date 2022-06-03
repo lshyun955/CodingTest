a18a9b6e323b30a284e2da48719a54be47877521
lshyun955 <lshyun955@gmail.com>
2022-06-03 18:59:51 +0900

    Add: 코드카타 2주차 5번째 문제 풀이 완료

---
+++ codekata/week2-day5.js
@@ -0,0 +1,13 @@
+function getMaxArea(height) {
+  // let max = Math.max(height[0],height[1]);
+  const result = [];
+
+  for (let i = 0; i < height.length; i++) {
+    for (let j = 1; j < height.length; j++) {
+      result.push(Math.min(height[i], height[j]) * (j - i));
+    }
+  }
+  return Math.max(...result);
+}
+
+console.log(getMaxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));
