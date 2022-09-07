d6c1e2b3dc7f7d6a128a511e7c4ba3d7b495021c
lshyun955 <lshyun955@gmail.com>
2022-09-07 18:37:19 +0900

    Add: 프로그래머스 N개의 최소공배수 풀이

---
+++ programmers/N개의 최소공배수.js
@@ -0,0 +1,16 @@
+function solution(arr) {
+  let answer = 0;
+  answer = arr.reduce((a, b) => leastCommonMultiple(a, b), 1);
+  return answer;
+}
+function leastCommonMultiple(a, b) {
+  return (a * b) / greatestCommonDivisor(a, b);
+}
+function greatestCommonDivisor(a, b) {
+  while (b > 0) {
+    let r = a % b;
+    a = b;
+    b = r;
+  }
+  return a;
+}
