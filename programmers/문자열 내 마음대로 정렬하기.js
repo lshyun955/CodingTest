2d4ecb2330aaa5853bcbc3c991d37ec37b5f716a
lshyun955 <lshyun955@gmail.com>
2022-10-25 23:23:20 +0900

    Add: 문자열 내 마음대로 정렬하기 풀이

---
+++ programmers/문자열 내 마음대로 정렬하기.js
@@ -0,0 +1,11 @@
+function solution(strings, n) {
+  let answer = strings.sort((a, b) => {
+    if (a[n] < b[n]) return -1;
+    if (a[n] > b[n]) return 1;
+    if (a[n] === b[n]) return a < b ? -1 : 1;
+    return 0;
+  });
+
+  return answer;
+}
+console.log(solution(["abce", "abcd", "cdx"], 2));
