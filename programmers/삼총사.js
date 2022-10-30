e39a1f0e3396cd35bde0cd52d713dd56712830d9
lshyun955 <lshyun955@gmail.com>
2022-10-30 23:52:54 +0900

    Add: 삼총사 문제 풀이 수정

--- programmers/삼총사.js
+++ programmers/삼총사.js
@@ -1,5 +1,10 @@
 function solution(number) {
   let answer = 0;
+  if (number.length === 3) {
+    const sum = number.reduce((acc, v) => acc + v, 0);
+    return sum === 0 ? 1 : 0;
+  } else {
+  }
   return answer;
 }
 
