bd290def50b9ee2b66803f9104914654af2154ae
lshyun955 <lshyun955@gmail.com>
2022-10-31 02:29:00 +0900

    Add: 소수 찾기 풀이

---
+++ programmers/소수 찾기.js
@@ -0,0 +1,17 @@
+function solution(n) {
+  const arr = Array(n + 1)
+    .fill(true)
+    .fill(false, 0, 2);
+
+  for (let i = 2; i * i <= n; i++) {
+    if (arr[i]) {
+      for (let j = i * i; j <= n; j += i) {
+        arr[j] = false;
+      }
+    }
+  }
+  return arr.filter((v) => v).length;
+}
+
+// console.log(solution(10));
+solution(10);
