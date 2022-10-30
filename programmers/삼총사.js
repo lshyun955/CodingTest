b0f9f54fa4dc4c475c2a6e5eaa1448ca9c82fb82
lshyun955 <lshyun955@gmail.com>
2022-10-31 01:03:51 +0900

    Add: 삼총사 문제 풀이 완료

--- programmers/삼총사.js
+++ programmers/삼총사.js
@@ -1,11 +1,21 @@
 function solution(number) {
-  let answer = 0;
+  const tempArr = [];
   if (number.length === 3) {
     const sum = number.reduce((acc, v) => acc + v, 0);
     return sum === 0 ? 1 : 0;
   } else {
+    const numArr = number.sort((a, b) => a - b);
+    for (let i = 0; i < numArr.length - 2; i++) {
+      for (let j = i + 1; j < numArr.length - 1; j++) {
+        for (let z = j + 1; z < numArr.length; z++) {
+          tempArr.push(numArr[i] + numArr[j] + numArr[z]);
+        }
+      }
+    }
   }
-  return answer;
+
+  return tempArr.filter((num) => num === 0).length;
 }
 
 console.log(solution([-2, 3, 0, 2, -5]));
+// 012 013 014 023 024 123 124 234
