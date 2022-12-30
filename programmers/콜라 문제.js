bf9a43beebc2d6e23fd5b83fe5e1f077e03477dd
lshyun955 <lshyun955@gmail.com>
2022-12-30 23:55:00 +0900

    콜라문제 해결

--- programmers/콜라 문제.js
+++ programmers/콜라 문제.js
@@ -1,4 +1,39 @@
-function solution(a, b, n) {
-  let answer = 0;
-  return answer;
+function solution(matrix, r) {
+  let tempArr = Array.from(Array(matrix.length), () =>
+    new Array(matrix[0].length).fill([])
+  );
+
+  tempArr = tempArr.map((arr, i) =>
+    arr.map((v, idx) => {
+      v = matrix[matrix.length - 1 - idx][i];
+      return v;
+    })
+  );
+  if (r % 4 === 0) return matrix;
+  if (r % 4 === 1) return tempArr;
+
+  if (r % 4 === 2) {
+    for (let i = 0; i < tempArr.length; i++) {
+      matrix[j].reverse();
+    }
+    matrix.reverse();
+    return matrix;
+  }
+  for (let i = 0; i < tempArr.length; i++) {
+    tempArr[i].reverse();
+  }
+  tempArr.reverse();
+
+  return tempArr;
 }
+
+console.log(
+  solution(
+    [
+      [4, 1, 2],
+      [7, 3, 4],
+      [3, 5, 6],
+    ],
+    3
+  )
+);
