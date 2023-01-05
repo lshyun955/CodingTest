7fc2c7d74fe297f486a6709e1c277e1bd98258ce
lshyun955 <lshyun955@gmail.com>
2023-01-06 00:29:52 +0900

    트라이 자동완성 풀이

---
+++ programmers/트라이_자동완성.js
@@ -0,0 +1,33 @@
+function makeTrie(words) {
+  const root = {};
+  for (const word of words) {
+    let current = root;
+    for (const char of word) {
+      if (!current[char]) current[char] = [0, {}];
+      current[char][0] = 1 + (current[char][0] || 0);
+      current = current[char][1];
+    }
+  }
+  return root;
+}
+
+function solution(words) {
+  let answer = 0;
+  const trie = makeTrie(words);
+
+  for (const word of words) {
+    let cnt = 0;
+    let current = trie;
+    for (const [_, char] of [...word].entries()) {
+      cnt++;
+      if (current[char][0] <= 1) {
+        break;
+      }
+      current = current[char][1];
+    }
+    answer += cnt;
+  }
+  return answer;
+}
+
+console.log(solution(["go", "gone", "guild"]));
