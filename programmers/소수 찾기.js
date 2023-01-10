d874b640da4e56813335949c5607c2ff250ad38c
lshyun955 <lshyun955@gmail.com>
2023-01-10 22:39:19 +0900

    소수 찾기 풀이 변경

--- programmers/소수 찾기.js
+++ programmers/소수 찾기.js
@@ -1,17 +1,12 @@
 function solution(n) {
-  const arr = Array(n + 1)
-    .fill(true)
-    .fill(false, 0, 2);
+  const prime = [false, false, ...Array(n - 1).fill(true)];
 
   for (let i = 2; i * i <= n; i++) {
-    if (arr[i]) {
-      for (let j = i * i; j <= n; j += i) {
-        arr[j] = false;
+    if (prime[i]) {
+      for (let j = i * 2; j <= n; j += i) {
+        prime[j] = false;
       }
     }
   }
-  return arr.filter((v) => v).length;
+  return prime.filter(Boolean).length;
 }
-
-// console.log(solution(10));
-solution(10);
