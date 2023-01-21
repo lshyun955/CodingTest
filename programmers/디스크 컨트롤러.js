4f836b7b567896c62bd57addb2b0ac02f7e63696
lshyun955 <lshyun955@gmail.com>
2023-01-22 00:08:14 +0900

    디스크 컨트롤러 최종

--- programmers/디스크 컨트롤러.js
+++ programmers/디스크 컨트롤러.js
@@ -7,79 +7,82 @@ class Heap {
     this.heap.push(value);
     let currentIndex = this.heap.length - 1;
     let parentIndex = Math.floor(currentIndex / 2);
-    console.log(currentIndex, parentIndex);
-    while (parentIndex !== 0 && this.heap[parentIndex][1] > value[1]) {
-      const temp = this.heap[parentIndex];
-      this.heap[parentIndex] = value;
-      this.heap[currentIndex] = temp;
-
+    while (
+      currentIndex > 1 &&
+      this.heap[parentIndex][1] > this.heap[currentIndex][1]
+    ) {
+      this.swap(parentIndex, currentIndex);
       currentIndex = parentIndex;
       parentIndex = Math.floor(currentIndex / 2);
     }
   }
 
   pop() {
-    if (this.heap.length === 2) return this.heap.pop();
     const returnValue = this.heap[1];
-    this.heap[1] = this.heap.pop();
+    if (this.heap.length <= 2) this.heap = [null];
+    else this.heap[1] = this.heap.pop();
 
     let currentIndex = 1;
-    let leftIndex = 2;
-    let rightIndex = 3;
+    let leftIndex = currentIndex * 2;
+    let rightIndex = currentIndex * 2 + 1;
+
+    if (!this.heap[leftIndex]) return returnValue;
+    if (!this.heap[rightIndex]) {
+      if (this.heap[leftIndex][1] < this.heap[currentIndex][1]) {
+        this.swap(leftIndex, currentIndex);
+      }
+      return returnValue;
+    }
 
     while (
-      this.heap[currentIndex] < this.heap[leftIndex] ||
-      this.heap[currentIndex] < this.heap[rightIndex]
+      this.heap[leftIndex][1] < this.heap[currentIndex][1] ||
+      this.heap[rightIndex][1] < this.heap[currentIndex][1]
     ) {
-      if (this.heap[leftIndex] < this.heap[rightIndex]) {
-        const temp = this.heap[currentIndex];
-        this.heap[currentIndex] = this.heap[rightIndex];
-        this.heap[rightIndex] = temp;
-        currentIndex = rightIndex;
-      } else {
-        const temp = this.heap[currentIndex];
-        this.heap[currentIndex] = this.heap[leftIndex];
-        this.heap[leftIndex] = temp;
-        currentIndex = leftIndex;
-      }
+      const minIdx =
+        this.heap[leftIndex][1] > this.heap[rightIndex][1]
+          ? rightIndex
+          : leftIndex;
+      this.swap(minIdx, currentIndex);
+
+      currentIndex = minIdx;
       leftIndex = currentIndex * 2;
       rightIndex = currentIndex * 2 + 1;
+
+      if (leftIndex >= this.size()) break;
     }
     return returnValue;
   }
-  top() {
-    return this.heap[1];
+
+  swap(a, b) {
+    [this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]];
   }
   size() {
     return this.heap.length - 1;
   }
-  empty() {
-    return this.heap.length === 1;
-  }
 }
 function solution(jobs) {
   const heapJobs = new Heap();
-  let answer = 0;
-  let idx = 0;
-  let time = 0;
+  const len = jobs.length;
   jobs.sort((a, b) => a[0] - b[0]);
-  for (let i = 0; i < jobs.length; i++) {
-    heapJobs.push(jobs[i]);
+
+  let answer = 0;
+  let time = 0;
+  let complete = 0;
+
+  while (jobs.length || heapJobs.size()) {
+    while (jobs.length) {
+      if (jobs[0][0] === time) {
+        heapJobs.push(jobs.shift());
+      } else break;
+    }
+    if (heapJobs.size() && time >= complete) {
+      const task = heapJobs.pop();
+      complete = task[1] + time;
+      answer += complete - task[0];
+    }
+    time++;
   }
-  // while (idx < jobs.length || !heapJobs.empty()) {
-  //   if (idx < jobs.length && jobs[idx][0] <= time) {
-  //     heapJobs.push(jobs[idx++]);
-  //     continue;
-  //   }
-  //   if (!heapJobs.empty()) {
-  //     time += heapJobs.top()[1];
-  //     answer += time - heapJobs.top()[0];
-  //     heapJobs.pop();
-  //   } else {
-  //     time = jobs[idx][0];
-  //   }
-  // }
-  // return Math.floor(answer / jobs.length);
+  return Math.floor(answer / len);
 }
 // 먼저 힙에 3 삽입
 // 1/ 2/ 3=1/ 2 /3 /4/ 5/ 6/ 7/ 8/ 9=1/ 2/ 3/ 4/ 5/ 6/
