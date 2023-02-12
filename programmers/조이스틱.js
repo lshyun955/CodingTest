9941588d311be1ca64b418ef34b6314d2ac8379b
lshyun955 <lshyun955@gmail.com>
2023-02-12 17:46:36 +0900

    조이스틱 map -> forEach 변경

--- programmers/조이스틱.js
+++ programmers/조이스틱.js
@@ -2,7 +2,7 @@ function solution(name) {
   let answer = 0;
   let min_move = name.length - 1;
 
-  [...name].map((n, i) => {
+  [...name].forEach((n, i) => {
     answer += Math.min(n.charCodeAt() - 65, 91 - n.charCodeAt());
     let idx = i + 1;
 
