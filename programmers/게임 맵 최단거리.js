e5bc37ab58478add7a12a5ebb00c142a803f157d
lshyun955 <lshyun955@gmail.com>
2023-02-19 20:39:40 +0900

    게임 맵 최단거리 풀이

--- programmers/게임 맵 최단거리.js
+++ programmers/게임 맵 최단거리.js
@@ -1,3 +1,37 @@
 function solution(maps) {
-  return answer;
+  const dx = [0, 0, 1, -1];
+  const dy = [1, -1, 0, 0];
+  const queue = [[0, 0, 1]];
+
+  while (queue.length) {
+    const curr = queue.shift();
+    if (curr[0] === maps.length - 1 && curr[1] === maps[0].length - 1) {
+      return curr[2];
+    }
+    for (let i = 0; i < 4; i++) {
+      const yy = curr[0] + dy[i];
+      const xx = curr[1] + dx[i];
+      if (
+        xx >= 0 &&
+        yy >= 0 &&
+        xx < maps[0].length &&
+        yy < maps.length &&
+        maps[yy][xx] === 1
+      ) {
+        maps[yy][xx] = 0;
+        queue.push([yy, xx, curr[2] + 1]);
+      }
+    }
+  }
+  return -1;
 }
+
+console.log(
+  solution([
+    [1, 0, 1, 1, 1],
+    [1, 0, 1, 0, 1],
+    [1, 0, 1, 1, 1],
+    [1, 1, 1, 0, 1],
+    [0, 0, 0, 0, 1],
+  ])
+);
