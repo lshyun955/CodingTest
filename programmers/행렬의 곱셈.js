e8b7115f67f7eb51012c1d049d149cba95f31705
lshyun955 <lshyun955@gmail.com>
2024-09-08 21:36:00 +0900

    수정: 행렬의 곱셈 풀이 추가

--- programmers/행렬의 곱셈.js
+++ programmers/행렬의 곱셈.js
@@ -1,3 +1,7 @@
+/**
+ * 풀이 1
+ */
+
 function solution(arr1, arr2) {
   const [row, col] = [arr1.length, arr2[0].length];
   let answer = new Array(row);
@@ -10,6 +14,25 @@ function solution(arr1, arr2) {
         0
       );
     }
+  }
+  return answer;
+}
+
+
+/**
+ * 풀이 2
+ */
+function solution(arr1, arr2) {
+  // [arr1.length x arr2[0].length] 행렬
+  const answer = Array.from({length: arr1.length}, () => Array(arr2[0].length).fill(0));
+  
+  // arr1[0][0] * arr2[0][0] + arr1[0][1] * arr2[1][0] ...
+  for (let i = 0; i < arr1.length; i++) {
+      for (let j = 0; j < arr2[0].length; j++) {
+          for (let k = 0; k < arr1[0].length; k++) {
+              answer[i][j] += arr1[i][k] * arr2[k][j];
+          }
+      }
   }
   return answer;
 }
