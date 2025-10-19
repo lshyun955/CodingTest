d89730a64ddb932cc3a155dc23914081cbcad505
PA_sh0530 <65072611+lshyun955@users.noreply.github.com>
2025-10-19 13:27:52 +0900

    Time: 1 ms (81.21%), Space: 58 MB (9.93%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.ts
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.ts
@@ -1,19 +1,16 @@
 function intersection(nums1: number[], nums2: number[]): number[] {
-    const interSectionSet: Set<number> = new Set();
-
     if (nums1.length > nums2.length) {
-        for (const num of nums2) {
-            if (nums1.includes(num)) {
-                interSectionSet.add(num);
-            }
-        }
-    } else {
-        for (const num of nums1) {
-            if (nums2.includes(num)) {
-                interSectionSet.add(num);
-            }
+        [nums1, nums2] = [nums2, nums1];
+    }
+
+    const setSmall = new Set(nums1);
+    const resultSet = new Set<number>();
+
+    for (const num of nums2) {
+        if (setSmall.has(num)) {
+            resultSet.add(num);
         }
     }
 
-    return [...interSectionSet];
+    return Array.from(resultSet);
 };
