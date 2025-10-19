4c1b5063781614641d461b686e8b4e8b6556553c
PA_sh0530 <65072611+lshyun955@users.noreply.github.com>
2025-10-19 20:54:12 +0900

    Time: 1 ms (38.04%), Space: 59 MB (42.39%) - LeetHub

---
+++ 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.ts
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function levelOrderBottom(root: TreeNode | null): number[][] {
+    if (!root) return [];
+    const queue = [root];
+    const result = [];
+
+    while(queue.length) {
+        const size = queue.length
+        const group = [];
+
+        for (let i = 0; i < size; i++) {
+            const curr = queue.shift();
+            group.push(curr.val);
+
+            if (curr.left) queue.push(curr.left)
+            if (curr.right) queue.push(curr.right)
+        }
+
+        result.push(group);
+    }
+
+    return result.reverse();
+};
