cafaed601ad8d7cc0379597786dac011cbf680e8
PA_sh0530 <65072611+lshyun955@users.noreply.github.com>
2025-10-21 23:27:11 +0900

    Time: 1 ms (77.06%), Space: 65 MB (16.51%) - LeetHub

---
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.ts
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function mergeTrees(root1: TreeNode | null, root2: TreeNode | null): TreeNode | null {
+    if (!root1 && !root2) {
+        return null;
+    }
+
+    const value1 = root1 ? root1.val : 0;
+    const value2 = root2 ? root2.val : 0;
+    const node = new TreeNode(value1 + value2);
+
+    node.left = mergeTrees(root1 ? root1.left : null, root2 ? root2.left : null);
+
+    node.right = mergeTrees(root1 ? root1.right : null, root2 ? root2.right : null);
+
+    return node;
+};
