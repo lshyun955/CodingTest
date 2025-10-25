d826f5decb8edc789bb96582d2984f62aa1a1440
PA_sh0530 <65072611+lshyun955@users.noreply.github.com>
2025-10-25 23:50:39 +0900

    Time: 12 ms (69.04%), Space: 60.3 MB (32.82%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.ts
+++ 0242-valid-anagram/0242-valid-anagram.ts
@@ -1,3 +1,16 @@
 function isAnagram(s: string, t: string): boolean {
-    return s.split('').sort((a,b)=> a.charCodeAt(0)- b.charCodeAt(0)).join('') === t.split('').sort((a,b)=> a.charCodeAt(0)- b.charCodeAt(0)).join('');
+    if (s.length !== t.length) return false;
+
+    const count: Record<string, number> = {};
+
+    for (const char of s) {
+        count[char] = (count[char] ?? 0) + 1;
+    }
+
+     for (const char of t) {
+        if (!count[char]) return false; // 해당 문자가 없거나 이미 다 소모됨
+        count[char]--;
+    }
+
+    return true;
 };
