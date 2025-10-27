64a6d5b73e275fdaf2062bb0324e91efdbe295e4
PA_sh0530 <65072611+lshyun955@users.noreply.github.com>
2025-10-27 14:28:39 +0900

    Time: 2 ms (74.33%), Space: 56.4 MB (70.55%) - LeetHub

--- 0001-two-sum/0001-two-sum.ts
+++ 0001-two-sum/0001-two-sum.ts
@@ -1,25 +1,14 @@
 function twoSum(nums: number[], target: number): number[] {
-    const numMap: Record<number, number[]> = {};
-    nums.forEach((num: number, idx: number) => {
-        if (!numMap[num]) {
-            numMap[num] = [idx];
-        } else {
-            numMap[num].push(idx);
-        }
-    });
+    const numMap: Record<number, number> = {};
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
 
-    for (const num of nums) {
-        const num1Idx = numMap[num][0];
-        let num2Idx = numMap[target - num]?.[0];
-
-        if (target - num === num && numMap[target - num].length > 1) {
-            num2Idx = numMap[num][1];
+        if (numMap[complement] !== undefined) {
+            return [numMap[complement], i];
         }
 
-        if (!num2Idx || num1Idx === num2Idx) {
-            continue;
-        }
-
-        return [num1Idx, num2Idx];
+        numMap[nums[i]] = i;
     }
+
+    return [];
 };
