b72d4b0585a4b53e8e5663fbd3802a09dd73824b
PA_sh0530 <65072611+lshyun955@users.noreply.github.com>
2025-10-27 10:59:13 +0900

    Time: 3 ms (91.02%), Space: 61.7 MB (84.19%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.ts
+++ 0012-integer-to-roman/0012-integer-to-roman.ts
@@ -1,28 +1,11 @@
 function intToRoman(num: number): string {
-    let result = '';
-    
-    const symbols: [number, string][] = [
-        [1000, 'M'],
-        [900, 'CM'],
-        [500, 'D'],
-        [400, 'CD'],
-        [100, 'C'],
-        [90, 'XC'],
-        [50, 'L'],
-        [40, 'XL'],
-        [10, 'X'],
-        [9, 'IX'],
-        [5, 'V'],
-        [4, 'IV'],
-        [1, 'I'],
-    ];
+    const thousands = ["", "M", "MM", "MMM"];
+    const hundreds  = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
+    const tens      = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
+    const ones      = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
 
-    for (const [value, symbol] of symbols) {
-        while (num >= value) {
-            result += symbol;
-            num -= value;
-        }
-    }
-
-    return result;
+    return thousands[Math.floor(num / 1000)] +
+           hundreds[Math.floor((num % 1000) / 100)] +
+           tens[Math.floor((num % 100) / 10)] +
+           ones[num % 10];
 };
