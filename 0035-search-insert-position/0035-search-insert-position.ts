17a403e31a1309c2f8ebf10941d236319090fd2c
PA_sh0530 <65072611+lshyun955@users.noreply.github.com>
2025-10-28 17:40:39 +0900

    Time: 0 ms (100%), Space: 54.2 MB (90.68%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.ts
+++ 0035-search-insert-position/0035-search-insert-position.ts
@@ -1,22 +1,16 @@
 function searchInsert(nums: number[], target: number): number {
-    if (nums.includes(target)) {
-        return nums.indexOf(target);
-    }
-
-    if (nums[nums.length - 1] < target ) {
-        return nums.length;
-    }
-
     let left = 0;
     let right = nums.length - 1;
 
     while (left <= right) {
         const mid = Math.floor((left + right) / 2);
 
-        if (nums[mid] > target) {
+        if (nums[mid] === target) {
+            return mid
+        } else if (nums[mid] > target) {
             right = mid - 1;
         } else {
-            left = mid +1;
+            left = mid + 1;
         }
     }
 
