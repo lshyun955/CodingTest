afde8a41e686367c0650c080b77a39d3eb9f4b1e
PA_sh0530 <65072611+lshyun955@users.noreply.github.com>
2025-11-03 19:04:41 +0900

    Time: 32 ms (88.89%), Space: 59.3 MB (80.56%) - LeetHub

--- 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.ts
+++ 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.ts
@@ -1,66 +1,30 @@
-class UnionFind {
-    private parent: number[];
-    private rank: number[];
-
-    constructor(n: number) {
-        this.parent = Array.from({ length: n }, (_, i) => i);
-        this.rank = Array(n).fill(0);
-    }
-
-    find(x: number): number {
-        if (this.parent[x] !== x) {
-            this.parent[x] = this.find(this.parent[x]);
-        }
-
-        return this.parent[x];
-    }
-
-    union(x: number, y: number): boolean {
-        const rootX = this.find(x);
-        const rootY = this.find(y);
-
-        if (rootX !== rootY) {
-            if (rootX > rootY) {
-                this.parent[rootY] = rootX;
-            } else if (rootX < rootY) {
-                this.parent[rootX] = rootY;
-            } else {
-                this.parent[rootY] = rootX;
-                this.rank[rootX]++;
-            }
-        } else {
-            return false;
-        }
-
-        return true;
-    }
-}
-
 function minCostConnectPoints(points: number[][]): number {
-    const length = points.length;
-    const uf = new UnionFind(length);
-    const edges: number[][] = [];
-    let answer: number = 0;
-    let edgeCount: number = 0;
+    const n = points.length;
+    const visited = new Array(n).fill(false);
+    const minDist = new Array(n).fill(Infinity);
+    minDist[0] = 0;
+    let answer = 0;
 
-    for (let i = 0; i < length; i++) {
-        for (let j = i + 1; j < length; j++) {
-            let cost = Math.abs(points[i][0] - points[j][0]) + Math.abs(points[i][1] - points[j][1]);
-            edges.push([cost, i, j]);
+    for (let i = 0; i < n; i++) {
+        let u = -1;
+        for (let j = 0; j < n; j++) {
+            if (!visited[j] && (u === -1 || minDist[j] < minDist[u])) {
+                u = j;
+            }
+        }
+
+        visited[u] = true;
+        answer += minDist[u];
+
+        for (let v = 0; v < n; v++) {
+            if (!visited[v]) {
+                const dist =
+                    Math.abs(points[u][0] - points[v][0]) +
+                    Math.abs(points[u][1] - points[v][1]);
+                minDist[v] = Math.min(minDist[v], dist);
+            }
         }
     }
 
-    edges.sort((a, b) => a[0] - b[0]);
-
-    for (const [weight, u, v] of edges) {
-        if (uf.union(u, v)) {
-            answer += weight;
-            edgeCount++;
-        }
-
-        if (edgeCount - 1 === length) {
-            return answer;
-        }
-    }
     return answer;
 };
